e7e20126df5c35b8497fbaaa6aa6a1a1d0ef079e
Arun Kumar <150873451+ArunKotu@users.noreply.github.com>
2025-11-13 18:51:46 +0530

    Update CORS configuration to include production origin
    Added allowed origin for production environment.

--- IntelliBlog/src/main/java/com/Intelli_Blog/IntelliBlog/Config/Cors.java
+++ IntelliBlog/src/main/java/com/Intelli_Blog/IntelliBlog/Config/Cors.java
@@ -14,7 +14,7 @@ public class Cors {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:5500", "http://localhost:3000", "http://127.0.0.1:5500", "http://localhost:8081")
+                        .allowedOrigins("https://intelliblog-6.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
